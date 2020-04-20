2bf698c2556aeb5e055cc7675ee997fd035c8477
Maxime <maxime.dutour@gmail.com>
2020-04-21 00:50:40 +0200

    MVC

---
+++ CountOnMe/View/Calculator.swift
@@ -0,0 +1,29 @@
+//
+//  Calculator.swift
+//  CountOnMe
+//
+//  Created by Maxime DUTOUR on 17/04/2020.
+//  Copyright © 2020 Vincent Saluzzo. All rights reserved.
+//
+
+import UIKit
+
+class Calculator: UIView {
+
+    
+    // déplacer les ib ici
+    // mettre la logic dans model
+    // a quoi sert le view controller
+    // comment mettre en place les notif
+    // pourquoi je ne peut pas ajouter ce fichier à un ib et est-ce necessaire
+    // comment faire interragir le tout
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
