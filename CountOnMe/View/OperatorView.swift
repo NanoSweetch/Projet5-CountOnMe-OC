e364ee6df1551a53e18c46da2644528878813e30
Maxime <maxime.dutour@gmail.com>
2020-04-26 17:47:20 +0200

    Sous classement vues partie 2

--- CountOnMe/View/OperatorView.swift
+++ CountOnMe/View/OperatorView.swift
@@ -10,12 +10,59 @@ import UIKit
 
 class OperatorView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    let calculateManager = CalculatesManager()
+    
+    let textDisplay = DisplayView()
+    
+    
+    var elements: [String] {
+        return textView.text.split(separator: " ").map { "\($0)" }
     }
-    */
-
+    
+    @IBAction func tappedAdditionButton(_ sender: UIButton) {
+        if calculateManager.canAddOperator(elements: elements) {
+            textView.text.append(Constants.addition.rawValue)
+        } else {
+           alertOperatorUsed()
+        }
+    }
+    
+    @IBAction func tappedSubstractionButton(_ sender: UIButton) {
+        if calculateManager.canAddOperator(elements: elements) {
+            textView.text.append(" - ")
+        } else {
+           alertOperatorUsed()
+        }
+    }
+    
+    @IBAction func tappedMultiplicationButton(_ sender: UIButton) {
+        if calculateManager.canAddOperator(elements: elements) {
+            textView.text.append(" × ")
+        } else {
+            alertOperatorUsed()
+        }
+    }
+    
+    @IBAction func tappedDivisionButton(_ sender: UIButton) {
+        if calculateManager.canAddOperator(elements: elements) {
+            textView.text.append(" ÷ ")
+        } else {
+            alertOperatorUsed()
+        }
+    }
+    
+    
+    @IBAction func tappedEqualButton(_ sender: UIButton) {
+        guard calculateManager.expressionIsCorrect(elements: elements) else {
+            return createAlert(message: Constants.enterCorrectExpression.rawValue)
+        }
+        
+        guard calculateManager.expressionHaveEnoughElement(elements: elements) else {
+            return createAlert(message: Constants.startNewCalcul.rawValue)
+        }
+        
+        let operationsToReduce = calculateManager.opertorToReduce(elements: elements)
+        
+        textView.text.append(" = \(operationsToReduce.first!)")
+    }
 }
